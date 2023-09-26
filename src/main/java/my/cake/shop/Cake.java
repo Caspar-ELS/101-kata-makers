32c3d0a76292e1d7f912545795a57c73c95a6b8c
Caspar-ELS <c.vandeursen@elsevier.com>
2023-09-26 15:37:27 +0100

    ok definitely no more pushes

--- src/main/java/my/cake/shop/Cake.java
+++ src/main/java/my/cake/shop/Cake.java
@@ -6,7 +6,7 @@ import my.cake.shop.exception.CakeException;
 
 public class Cake {
 
-  private static final long FIVE_SECONDS = 5L;
+  private static final int EXPIRY_TIME_IN_SECONDS = 5;
 
   @Getter
   private final Size size;
@@ -18,14 +18,14 @@ public class Cake {
   private String name;
 
   @Getter
-  private final long expiryInSeconds;
+  private final long momentOfExpiryInUnixTime;
 
   public Cake(Size size, Colors color, Type type) {
     this.size = size;
     this.color = color;
     this.type = type;
 
-    this.expiryInSeconds = (System.currentTimeMillis() / 1000L) + FIVE_SECONDS;
+    this.momentOfExpiryInUnixTime = getCurrentUnixTime() + EXPIRY_TIME_IN_SECONDS;
   }
 
   public String getName() {
@@ -50,11 +50,15 @@ public class Cake {
   }
 
   public long expiresInSeconds() {
-    long result = (expiryInSeconds - System.currentTimeMillis() / 1000L);
+    long result = momentOfExpiryInUnixTime - getCurrentUnixTime();
     return result < 0 ? 0 : result;
   }
 
   public void rename(String newName) {
     this.name = newName;
+  }
+
+  private static long getCurrentUnixTime() {
+    return System.currentTimeMillis() / 1000L;
   }
 }
