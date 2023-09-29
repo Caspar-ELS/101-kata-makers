4766a4b2fc907a751d6f92700e4e115d8415449c
JasonKwong <j.kwong+copilot@elsevier.com>
2023-09-29 13:15:32 +0100

    add kata code

--- src/main/java/my/cake/shop/Cake.java
+++ src/main/java/my/cake/shop/Cake.java
@@ -1,5 +1,56 @@
 package my.cake.shop;
 
-public class Cake {
+import java.util.Date;
 
+public class Cake {
+  public String createCake(String color, String type, String size) {
+    return createCake(color, type, size, "");
+  }
+
+  public String createCake(String color, String type, String size, String specialName) {
+    if(specialName.isEmpty()){
+      return String.format("%s %s %s cake", size, color, type);
+    }
+    return specialName;
+  }
+
+  public String getThePrice(Integer sPrice, Integer mPrice, Integer lPrice, String desiredSize){
+
+    if(outOfBounds(sPrice) || outOfBounds(mPrice) || outOfBounds(lPrice)){
+      return "Error, price out of bounds";
+    }
+
+    if(desiredSize.equals("small")){
+      return String.format("The price is %d", sPrice);
+    }
+    if(desiredSize.equals("medium")){
+      return String.format("The price is %d", mPrice);
+    }
+    if(desiredSize.equals("large")){
+      return String.format("The price is %d", lPrice);
+    }
+    return "Error";
+  }
+
+  public Integer getFreshMinutesLeft(Date cakeCreatedTime, Date currentTime) throws Exception {
+    if(cakeCreatedTime.after(currentTime)){
+      throw new Exception("Cake created time is after current time");
+    }
+
+    long timeDiff = currentTime.getTime() - cakeCreatedTime.getTime();
+    System.out.println("cakeCreatedDate: " + cakeCreatedTime);
+    System.out.println("current time: " + new Date());
+    System.out.println("timeDiff: " + timeDiff);
+
+    if ( 3600000 >= timeDiff){
+      return (int) (timeDiff / 1000 / 60);
+    }else{
+      throw new Exception("Cake is not fresh");
+    }
+  }
+
+
+  private boolean outOfBounds(Integer price){
+    return price < 20 || price > 50;
+  }
 }
--- src/test/java/my/cake/shop/CakeTest.java
+++ src/test/java/my/cake/shop/CakeTest.java
@@ -1,8 +1,90 @@
 package my.cake.shop;
 
+import java.util.Date;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CakeTest {
 
+  @Test
+  @Disabled
+  void createCake() {
+    Assertions.assertEquals(new Cake().createCake("Red", "Cheese", "Large"),
+        "Cake created with color Red, type Cheese, and size Large");
+  }
+
+  @Test
+  void largeOrangeCheeseCake() {
+    Assertions.assertEquals(new Cake().createCake("orange", "cheese", "large"),
+        "large orange cheese cake");
+  }
+
+  @Test
+  void mediumBlueCreamCake() {
+    Assertions.assertEquals(new Cake().createCake("blue", "cream", "medium"),
+        "medium blue cream cake");
+  }
+
+  @Test
+  void smallRedFruitCake() {
+    Assertions.assertEquals(new Cake().createCake("red", "fruit", "small"),
+        "small red fruit cake");
+  }
+
+  @Test
+  void withSpecialName() {
+    final String SOME_RANDOM_NAME = "This will be a super cake made by BOM";
+    Assertions.assertEquals(
+        new Cake().createCake("someColor", "someType", "someSize", SOME_RANDOM_NAME),
+        SOME_RANDOM_NAME);
+  }
+
+  // withinMinMax
+  @Test
+  void withinMinMax(){
+    Assertions.assertEquals(new Cake().getThePrice(20, 30, 40, "small"), "The price is 20");
+  }
+
+  // too large
+  @Test
+  void tooLarge(){
+    Assertions.assertEquals(new Cake().getThePrice(1000, 30, 40, "small"), "Error, price out of bounds");
+  }
+
+  // too small
+  @Test
+  void tooSmall(){
+    Assertions.assertEquals(new Cake().getThePrice(19, 30, 40, "small"), "Error, price out of bounds");
+  }
+
+  // onMax
+  @Test
+  void onMax(){
+    Assertions.assertEquals(new Cake().getThePrice(50, 50, 50, "small"), "The price is 50");
+  }
+
+  // onMin
+  @Test
+  void onMin(){
+    Assertions.assertEquals(new Cake().getThePrice(20, 20, 20, "small"), "The price is 20");
+  }
+
+  // fresh
+  @Test
+  void fresh() throws Exception {
+    Date cakeCreatedAt = new Date(123, 8, 26, 14, 00);
+    Date currentTime = new Date(123, 8, 26, 14, 07);
+    Assertions.assertEquals(new Cake().getFreshMinutesLeft(cakeCreatedAt, currentTime), 7);
+  }
+
+  // not fresh
+  @Test
+  void notFresh() throws Exception {
+    Date cakeCreatedAt = new Date(123, 8, 26, 14, 00);
+    Date currentTime = new Date(123, 8, 26, 16, 00);
+    Assertions.assertThrows(Exception.class, () -> {
+      new Cake().getFreshMinutesLeft(cakeCreatedAt, currentTime);
+    });
+  }
 }
