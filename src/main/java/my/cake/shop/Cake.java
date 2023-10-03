0d151cea3a88c326251c716715db5d94c03ceb79
laurenashton <l.ashton@elsevier.com>
2023-10-03 09:14:54 +0100

    Java 101 26/09/23

--- src/main/java/my/cake/shop/Cake.java
+++ src/main/java/my/cake/shop/Cake.java
@@ -1,5 +1,51 @@
 package my.cake.shop;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cake {
 
+  private String colour;
+  private String type;
+  private String size;
+  private String name;
+  private Long timeCreated;
+
+
+  public Cake(String colour, String type, String size) {
+    this.colour = colour;
+    this.type = type;
+    this.size = size;
+    this.timeCreated = System.currentTimeMillis();
+  }
+
+  public Cake(String colour, String type, String size, String name) {
+    this.colour = colour;
+    this.type = type;
+    this.size = size;
+    this.name = name;
+    this.timeCreated = System.currentTimeMillis();
+  }
+
+  public String printCakeName(Cake cake) {
+    if (cake.getName() != null) {
+      return cake.getName();
+    }
+    return cake.getSize() + " " + cake.getColour() + " " + cake.getType();
+  }
+
+  public String getPrice(String cakeSize) {
+    return cakeSize.equals("large") ? "£50" : "£20";
+  }
+
+  public String getCakeExpiry(Long timeCreated) {
+    long timeDifference = (System.currentTimeMillis() - timeCreated) / 1000L;
+    if (timeDifference <= 5) {
+      return "Your cake will expire in " + timeDifference + " seconds";
+    } else {
+      return "Your cake expired :(";
+    }
+  }
 }
--- src/test/java/my/cake/shop/CakeTest.java
+++ src/test/java/my/cake/shop/CakeTest.java
@@ -1,8 +1,83 @@
 package my.cake.shop;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class CakeTest {
+
+  @Test
+  void willCreateAnyCake() {
+    Cake cake = new Cake("Blue", "Chocolate", "Medium");
+
+    assertEquals("Medium", cake.getSize());
+    assertEquals("Chocolate", cake.getType());
+    assertEquals("Blue", cake.getColour());
+  }
+
+  @Test
+  void willOutputTheNameOfACake() {
+    Cake cake = new Cake("orange", "cheese cake", "large");
+
+    String expectedOutput = "large orange cheese cake";
+    String actualOutput = cake.printCakeName(cake);
+
+    assertEquals(expectedOutput, actualOutput);
+  }
+
+  @Test
+  void willBeAbleToOverrideTheDefaultCakeName() {
+    Cake cake = new Cake("orange", "cheese cake", "large", "Caspar's amazing cake");
+
+    String expectedCakeName = "Caspar's amazing cake";
+    String actualCakeName = cake.printCakeName(cake);
+
+    assertEquals(expectedCakeName, actualCakeName);
+  }
+
+  @Test
+  void willGetTheCorrectPriceOfALargeCake() {
+    Cake cake = new Cake("orange", "cheese cake", "large");
+
+    String expectedPrice = "£50";
+    String actualPrice = cake.getPrice(cake.getSize());
+
+    assertEquals(expectedPrice, actualPrice);
+
+  }
+
+  @Test
+  void willGetTheCorrectPriceOfASmallCake() {
+    Cake cake = new Cake("orange", "cheese cake", "small");
+
+    String expectedPrice = "£20";
+    String actualPrice = cake.getPrice(cake.getSize());
+
+    assertEquals(expectedPrice, actualPrice);
+
+  }
+
+  @Test
+  void willOutputHowManySecondsLeftBeforeTheCakeExpiresWhenCalledAfterLessThan5Seconds() throws InterruptedException {
+    Cake cake = new Cake("orange", "cheese cake", "small");
+
+    Thread.sleep(3000);
+    String expectedSecondsLeft = "Your cake will expire in 3 seconds";
+    String actualSecondsLeft = cake.getCakeExpiry(cake.getTimeCreated());
+
+    assertEquals(expectedSecondsLeft, actualSecondsLeft);
+
+  }
+
+  @Test
+  void willOutputThatTheCakeExpiredWhenLongerThan5SecondsHasPassed() throws InterruptedException {
+    Cake cake = new Cake("orange", "cheese cake", "small");
+
+    Thread.sleep(6000);
+    String expectedOutput = "Your cake expired :(";
+    String actualOutput = cake.getCakeExpiry(cake.getTimeCreated());
+
+    assertEquals(expectedOutput, actualOutput);
+
+  }
 
 }
