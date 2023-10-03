9db2f37a75165535586b7773902ce5c98a1cf7a7
ewa-j <e.jablonska@elsevier.com>
2023-10-03 10:04:34 +0100

    cake

--- src/main/java/my/cake/shop/Cake.java
+++ src/main/java/my/cake/shop/Cake.java
@@ -1,5 +1,66 @@
 package my.cake.shop;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Cake {
 
+  private static final int TIME_TO_LIVE_MS = 5000;
+  private Color color;
+  private Size size;
+  private Type type;
+  private String name;
+  private Double price;
+  private long timeCakeMade;
+
+  public static Cake makeCake(Color color, Size size, Type type) {
+    Cake cake = new Cake();
+    cake.setColor(color);
+    cake.setSize(size);
+    cake.setType(type);
+    cake.setName(color, size, type);
+    cake.setTimeCakeMade(System.currentTimeMillis());
+    return cake;
+  }
+
+  public static Cake makeCake(Color color, Size size, Type type, String name) {
+    Cake cake = new Cake();
+    cake.setColor(color);
+    cake.setSize(size);
+    cake.setType(type);
+    cake.setName(name);
+    cake.setTimeCakeMade(System.currentTimeMillis());
+    return cake;
+  }
+
+  public void setName(Color color, Size size, Type type) {
+    name = size.name() + " " + color.name() + " " + type.name();
+  }
+
+  public Double setPriceBasedOnSize() {
+    Cake cake = makeCake(color, size, type);
+    if (cake.getSize().equals(Size.SMALL)) {
+      cake.setPrice(20.00);
+    } else if (cake.getSize().equals(Size.MEDIUM)) {
+      cake.setPrice(35.00);
+    } else if (cake.getSize().equals(Size.LARGE)) {
+      cake.setPrice(50.00);
+    }
+    return cake.getPrice();
+  }
+
+  public static String checkHowMuchTimeLeft(Cake cake) {
+    long stop = System.currentTimeMillis();
+    long timePassed = stop - cake.getTimeCakeMade();
+    long timeLeft;
+
+    if (timePassed > TIME_TO_LIVE_MS) {
+      return "Expired";
+    } else {
+      timeLeft = TIME_TO_LIVE_MS - timePassed;
+      return "Time left: " + timeLeft + " milliseconds";
+    }
+  }
 }
--- src/test/java/my/cake/shop/CakeTest.java
+++ src/test/java/my/cake/shop/CakeTest.java
@@ -1,8 +1,87 @@
 package my.cake.shop;
 
+import java.util.concurrent.TimeUnit;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CakeTest {
 
+  @Test
+  void shouldHaveColor() {
+    Cake cake = new Cake();
+    cake.setColor(Color.BLUE);
+    Assertions.assertEquals(Color.BLUE, cake.getColor());
+  }
+
+  @Test
+  void shouldHaveType() {
+    Cake cake = new Cake();
+    cake.setType(Type.CHEESECAKE);
+    Assertions.assertEquals(Type.CHEESECAKE, cake.getType());
+  }
+
+  @Test
+  void shouldHaveSize() {
+    Cake cake = new Cake();
+    cake.setSize(Size.SMALL);
+    Assertions.assertEquals(Size.SMALL, cake.getSize());
+  }
+
+  @Test
+  void shouldHaveName() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+    Assertions.assertEquals("LARGE ORANGE CHEESECAKE", cake.getName());
+  }
+
+  @Test
+  void shouldOverwriteDefaultNameIfNameChangedAfterMakingCake() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+    cake.setName("Amazing cheesecake");
+    Assertions.assertEquals("Amazing cheesecake", cake.getName());
+  }
+
+  @Test
+  void shouldMakeCakeWithCustomName() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE, "Amazing cheesecake");
+    Assertions.assertEquals("Amazing cheesecake", cake.getName());
+  }
+
+  @Test
+  void smallCakeShouldCost20() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.SMALL, Type.CHEESECAKE);
+    Assertions.assertEquals(20.00, cake.setPriceBasedOnSize());
+  }
+
+  @Test
+  void mediumCakeShouldCost35() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.MEDIUM, Type.CHEESECAKE);
+    Assertions.assertEquals(35.00, cake.setPriceBasedOnSize());
+  }
+
+  @Test
+  void largeCakeShouldCost50() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+    Assertions.assertEquals(50.00, cake.setPriceBasedOnSize());
+  }
+
+  @Test
+  void shouldReturnFullTimeLeftRightAfterMakingCake() {
+    Cake cake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+    Assertions.assertEquals("Time left: 5000 milliseconds", Cake.checkHowMuchTimeLeft(cake));
+  }
+
+//  @Test
+//  void shouldNotReturnFullTimeLeftSomeTimeAfterMakingCake() {
+//    Cake anotherCake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+//    Awaitility.waitAtMost(20, TimeUnit.SECONDS);
+//    Assertions.assertNotEquals("Time left: 5000 milliseconds", Cake.checkHowMuchTimeLeft(anotherCake));
+//  }
+
+  @Test
+  void shouldReturnExpired() {
+    Cake newCake = Cake.makeCake(Color.ORANGE, Size.LARGE, Type.CHEESECAKE);
+    Awaitility.await().pollDelay(6, TimeUnit.SECONDS)
+        .untilAsserted(() -> Assertions.assertEquals("Expired", Cake.checkHowMuchTimeLeft(newCake)));
+  }
 }
