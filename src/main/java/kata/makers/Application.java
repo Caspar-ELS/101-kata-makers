6c3bba507cf40ef60d3044452c7e890992843d99
JasonKwong <j.kwong+copilot@elsevier.com>
2024-01-09 11:22:25 +0000

    update sample code

--- src/main/java/kata/makers/Application.java
+++ src/main/java/kata/makers/Application.java
@@ -1,9 +1,9 @@
 package kata.makers;
 
+import java.util.Scanner;
 import kata.makers.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,8 +16,8 @@ public class Application implements CommandLineRunner {
   @Autowired
   DemoService demoService;
 
-  @Value("${param.input}")
   String input;
+  String secondInput;
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
@@ -27,6 +27,22 @@ public class Application implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
+    // sample code for getting input
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What your input?");
+    if (scanner.hasNext()) {
+      input = scanner.nextLine();
+    }
+
+    System.out.println("What your second input?");
+    if (scanner.hasNext()) {
+      secondInput = scanner.nextLine();
+    }
+
+    log.info("Input: {}", input);
+    log.info("Second Input: {}", secondInput);
+
+    // sample code for autowiring a service
     demoService.foo();
   }
 }
