20a7e5ae474feb526c2c7e442c1cfe82b48b0832
Caspar-ELS <c.vandeursen@elsevier.com>
2024-03-15 14:25:08 +0000

    added two working modules

--- src/main/java/kata/makers/HelloWorld.java
+++ src/main/java/kata/makers/HelloWorld.java
@@ -1,7 +1,16 @@
 package kata.makers;
 
+import static com.amazonaws.auth.policy.actions.S3Actions.ListObjects;
+
+import lombok.Getter;
+
 public class HelloWorld {
-  public static String greet() {
-    return "Hello world!";
+
+  @Getter
+  private String hello;
+
+  public static void main(String[] args) {
+ListObjects
+
   }
 }
