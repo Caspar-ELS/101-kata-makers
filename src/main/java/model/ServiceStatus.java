279cc350be5c514db7b97e85bcc1cfc0fac71159
JasonKwong <j.kwong+copilot@elsevier.com>
2024-08-28 16:00:42 +0100

    create model for ec2 status

---
+++ src/main/java/model/ServiceStatus.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
+public class ServiceStatus {
+  private String name;
+  private boolean isRunning;
+}
