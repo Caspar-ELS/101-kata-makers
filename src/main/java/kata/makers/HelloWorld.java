3245d0f109b72c25dc89bc0c89cd82a08195585a
Caspar-ELS <c.vandeursen@elsevier.com>
2026-01-30 11:26:27 +0000

    commits

--- src/main/java/kata/makers/HelloWorld.java
+++ src/main/java/kata/makers/HelloWorld.java
@@ -1,7 +1,27 @@
 package kata.makers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld {
-  public static String greet() {
-    return "Hello world!";
+
+  private static final List<String> allCities = List.of("Paris", "Budapest", "Skopje", "Rotterdam",
+      "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London",
+      "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul");
+
+  public static List<String> findCity(String find) {
+    if (find.equals("*")) return allCities;
+    if (find.length() < 2) return null;
+
+    List<String> found = new ArrayList<>();
+
+    allCities.forEach(city -> {
+      if (city.toLowerCase().contains(find.toLowerCase())) {
+        found.add(city);
+      }
+    });
+
+    return found;
+
   }
 }
--- src/test/java/kata/makers/HelloWorldTest.java
+++ src/test/java/kata/makers/HelloWorldTest.java
@@ -1,13 +1,67 @@
 package kata.makers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
+  private static final List<String> cities = List.of("Paris", "Budapest", "Skopje", "Rotterdam",
+      "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London",
+      "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul");
+
   @Test
-  public void shouldGreet() {
-    assertEquals(HelloWorld.greet(), "Hello world!");
+  public void shouldReturnNullWhenQueryTooShort() {
+    assertNull(HelloWorld.findCity(""));
+    assertNull(HelloWorld.findCity("a"));
+    assertNotNull(HelloWorld.findCity("ab"));
+  }
+
+  @Test
+  public void shouldFindCityStartsWith() {
+    cities.forEach(city -> {
+
+      String shortCity = city.substring(0, 2);
+      System.out.println("Now testing city: " + city + ", short: " + shortCity);
+
+      List<String> result = HelloWorld.findCity(shortCity);
+      assertTrue(result.contains(city));
+
+    });
+  }
+
+  @Test
+  public void shouldFindCitySubstringAnywhere() {
+    cities.forEach(city -> {
+
+      String shortCity = city.substring(2);
+      System.out.println("Now testing city: " + city + ", short: " + shortCity);
+
+      List<String> result = HelloWorld.findCity(shortCity);
+      assertTrue(result.contains(city));
+
+    });
+  }
+
+  @Test
+  public void shouldBeCaseInsensitive() {
+    List<String> result = HelloWorld.findCity("pa");
+    assertTrue(result.contains("Paris"));
+  }
+
+  @Test
+  public void shouldReturnAllOnAsterisk() {
+    List<String> result = HelloWorld.findCity("*");
+    assertEquals(result, cities);
+  }
+
+  @Test
+  public void shouldThrowOnNull() {
+    assertThrows(NullPointerException.class, () -> HelloWorld.findCity(null));
   }
 }
